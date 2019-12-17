9497ab73310b84f82d323ed10dbb07f772a0780a
gonzalopla <gonzalo_p@csu.fullerton.edu>
2019-12-16 20:28:19 -0800

    Update selectionsort.js

--- selectionsort.js
+++ selectionsort.js
@@ -1,27 +1,15 @@
-let selectionSort = (items) => {
+let selectionSort = (data) => {
 
-	//Loop till the second last element
-	for(let i = 0; i < items.length - 1; i++){
+	for(let i = 0; i < data.length - 1; i++){
  
-	   //Loop after the i till the last element
-	   for(let j = i + 1; j < items.length; j++){
- 
-		  //if jth element is less than the ith element then swap
-		  //change < to > for sorting in descending order
-		  if(items[j] < items[i]){
-			let temp = items[i];
-			items[i] = items[j];
-			items[j] = temp;
-		  }
- 
-	   }
- 
-	}
-	
-	//return the sorted array
-	return items;
- }
- if(items[j] < items[i]){
-	[ items[i], items[j] ] = [ items[j], items[i] ];
-  }
+	   for(let j = i + 1; j < data.length; j++){
+
+		  if(data[j] < data[i]){
+			let holder = data[i];
+			data[i] = data[j];
+			data[j] = holder; }}}
+
+	return data; }
+ if(data[j] < data[i]){
+	[ data[i], data[j] ] = [ data[j], data[i] ]; }
 
